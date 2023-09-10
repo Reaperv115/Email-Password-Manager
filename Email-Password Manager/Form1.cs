ce5ddcfe46e8d28821e679f1b5cc2d72635b3364
Slaygony <rjs5785@hotmail.com>
2023-09-10 14:51:34 -0400

    user can now load the file into the datagridview.

--- Email-Password Manager/Form1.cs
+++ Email-Password Manager/Form1.cs
@@ -15,6 +15,7 @@ namespace WindowsFormsApp1
     {
         string savefileName = "/savefile.docx";
         string savefilePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        DataTable table = new DataTable();
         public Form1()
         {
             InitializeComponent();
@@ -24,8 +25,8 @@ namespace WindowsFormsApp1
         private void AddName_Click(object sender, EventArgs e)
         {
             string[] entries = new string[3];
-            entries[0] = EmailAddress.Text.Trim();
-            entries[1] = Password.Text.Trim();
+            entries[0] = WebsiteName.Text.Trim();
+            entries[1] = EmailAddress.Text.Trim();
             entries[2] = Password.Text.Trim();
             string[] row = { entries[0], entries[1], entries[2] };
             dataGridView1.Rows.Add(row);
@@ -34,7 +35,7 @@ namespace WindowsFormsApp1
                 WebsiteName.Text = "";
             EmailAddress.Text = "";
             Password.Text = "";
-
+            //dataGridView1.DataSource = table;
         }
 
         private void SaveBtn_Click(object sender, EventArgs e)
@@ -63,21 +64,28 @@ namespace WindowsFormsApp1
             }
 
             File.Create(filepath + savefileName).Dispose();
-            StreamWriter streamWriter = new StreamWriter(filepath + savefileName, false);
-            //foreach (object obj in listView1.Items)
+            using (TextWriter tw = new StreamWriter(filepath + savefileName))
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count; ++i)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; ++j)
+                    {
+                        tw.Write(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                        if (j != dataGridView1.Columns.Count)
+                        {
+                            tw.Write(" ");
+                        }
+                    }
+                    tw.WriteLine();
+                }
+            }
+            //StreamWriter streamWriter = new StreamWriter(filepath + savefileName, false);
+            //foreach (object obj in dataGridView1.Rows)
             //{
             //    streamWriter.WriteLine(obj.ToString());
             //}
-            try
-            {
-                File.Encrypt(filepath + savefileName);
-            }
-            catch (Exception ex) 
-            { 
-                Console.Write(ex.ToString(), "file is not able to be encrypted.");
-                Console.Write("You must be running the pro version of windows 11 to encrypt files.");
-            }
-            streamWriter.Close();
+            
+            //streamWriter.Close();
         }
 
         private void LoadBtn_Click(object sender, EventArgs e)
@@ -86,6 +94,7 @@ namespace WindowsFormsApp1
                 Console.WriteLine("Error: file does not exist");
             else
             {
+                
                 try
                 {
                     File.Decrypt(savefilePath + savefileName);
@@ -95,29 +104,37 @@ namespace WindowsFormsApp1
                     
                     Console.Write(ex.ToString(), "file is not able to be decrypted.");
                 }
-                FileStream fileStream = File.OpenRead(savefilePath + savefileName);
-                using (StreamReader streamReader = new StreamReader(fileStream))
+                string[] lines = File.ReadAllLines(savefilePath + savefileName);
+                string[] values;
+
+                for (int i = 0; i < lines.Length; ++i)
                 {
-                    string line;
-                    //while ((line = streamReader.ReadLine()) != null)
-                    //    listView1.Items.Add(line);
+                    values = lines[i].ToString().Split(' ');
+                    string[] row = new string[values.Length - 1];
+                    for (int j = 0; j < values.Length - 1; ++j)
+                    {
+                        row[j] = values[j].Trim();
+                    }
+                    table.Rows.Add(row);
                 }
+                dataGridView1.DataSource = table;
+                
             }
         }
 
         private void InitializeColumns()
         {
             dataGridView1.AllowUserToAddRows = false;
-            DataGridViewColumn column1 = new DataGridViewTextBoxColumn();
-            DataGridViewColumn column2 = new DataGridViewTextBoxColumn();
-            DataGridViewColumn column3 = new DataGridViewTextBoxColumn();
-            column1.Name = "Website(optional)";
-            column2.Name = "Username/Email";
-            column3.Name = "Password";
-            dataGridView1.Columns.Add(column1);
-            dataGridView1.Columns.Add(column2);
-            dataGridView1.Columns.Add(column3);
+            DataColumn column1 = new DataColumn();
+            DataColumn column2 = new DataColumn();
+            DataColumn column3 = new DataColumn();
+            column1.ColumnName = "Website(optional)";
+            column2.ColumnName = "Username/Email";
+            column3.ColumnName = "Password";
+            table.Columns.Add(column1);
+            table.Columns.Add(column2);
+            table.Columns.Add(column3);
         }
-        
+
     }
 }
